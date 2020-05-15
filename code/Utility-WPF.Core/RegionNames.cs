5b742fe5ea9d353ea8b467cbc9ce47fcc6006ed3
Alex Hedley <alex.hedley@me.com>
2020-05-15 08:28:21 +0100

    Added helpers to Core

---
+++ code/Utility-WPF.Core/RegionNames.cs
@@ -0,0 +1,10 @@
+﻿namespace Utility_WPF.Core
+{
+    /// <summary>
+    /// Region Names
+    /// </summary>
+    public static class RegionNames
+    {
+        public const string ContentRegion = "ContentRegion";
+    }
+}
