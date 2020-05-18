98fe0af481b98d18444f38cf48cfacb2b56b140e
Alex Hedley <alex.hedley@me.com>
2020-05-18 17:38:50 +0100

    #9 Added initial shells for other service tests

---
+++ code/Utility-WPF.Services.Tests/JSONServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Utility_WPF.Services.Interfaces;
+
+namespace Utility_WPF.Services.Tests
+{
+    /// <summary>
+    /// JSON Service - Tests
+    /// </summary>
+    [TestClass]
+    public class JSONServiceTests
+    {
+        #region Properties
+
+        IJSONService _jsonService;
+
+        #endregion Properties
+
+        /// <summary>
+        /// JSON Service - Tests
+        /// </summary>
+        public JSONServiceTests()
+        {
+            _jsonService = new JSONService();
+        }
+
+        //[TestMethod]
+        //public void PrettyJSON()
+        //{
+        //}
+    }
+
+}
---
+++ code/Utility-WPF.Services.Tests/XMLServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Utility_WPF.Services.Interfaces;
+
+namespace Utility_WPF.Services.Tests
+{
+    /// <summary>
+    /// XML Service - Tests
+    /// </summary>
+    [TestClass]
+    public class XMLServiceTests
+    {
+        #region Properties
+
+        IXMLService _xmlService;
+
+        #endregion Properties
+
+        /// <summary>
+        /// XML Service - Tests
+        /// </summary>
+        public XMLServiceTests()
+        {
+            _xmlService = new XMLService();
+        }
+
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    var xml = @"<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>";
+        //    var formattedXml = _xmlService.PrettyXML(xml);
+        //}
+    }
+}
