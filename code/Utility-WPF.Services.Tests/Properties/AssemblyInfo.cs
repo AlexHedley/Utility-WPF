13643acf30ca55054ad07f147f320195a01e8ec8
Alex Hedley <alex.hedley@me.com>
2020-05-18 17:33:28 +0100

    #9 Initial tests project

---
+++ code/Utility-WPF.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Utility-WPF.Services.Tests")]
+[assembly: AssemblyDescription("Utility WPF Services Tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alex Hedley")]
+[assembly: AssemblyProduct("Utility")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8df2b804-157f-463d-ad89-330c75900ac6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
